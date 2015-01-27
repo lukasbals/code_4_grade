abeaba03d1e3c6bbbf82cc052d3c10145c49e7fd
lukasbals <lukibals@gmx.at>
2015-01-27 16:41:02 +0100

    ok

--- 08_REST_Test/src/at/bals/tastkservice/services/TaskService.java
+++ 08_REST_Test/src/at/bals/tastkservice/services/TaskService.java
@@ -1,13 +1,15 @@
 package at.bals.tastkservice.services;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -15,33 +17,56 @@ import at.bals.taskservice.vo.Task;
 
 @Path("tasks/")
 public class TaskService {
+	private Connection connection;
+	private List<Task> taskList = new ArrayList<Task>();
+
 	@GET
 	@Path("")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public List<Task> getAllTasks() {
-		Task t1 = new Task("do it", "now");
-		Task t2 = new Task("dont do it", "never");
+		
+		if (this.connection == null) {
+			setConnection();
+		}
 
-		List<Task> taskList = new ArrayList<Task>();
-		taskList.add(t1);
-		taskList.add(t2);
+		getData();
 
 		return taskList;
 	}
 
-	@GET
-	@Path("{id}")
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public Task getTaskForId(@PathParam("id") int id) {
-		Task t = new Task("id task", "id " + id);
-		return t;
+	private void getData() {
+		taskList.clear();
+		try {
+			Statement stmt = this.connection.createStatement();
+
+			ResultSet resultSet = stmt.executeQuery("select * from firstRest;");
+			resultSet.first();
+			while (!(resultSet.isAfterLast())) {
+				Task t = new Task(resultSet.getString(1),
+						resultSet.getString(2));
+				taskList.add(t);
+				resultSet.next();
+			}
+			stmt.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	@POST
-	@Path("")
-	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	@Consumes(MediaType.APPLICATION_XML)
-	public Task addTask(Task task) {
-		return task;
+	private void setConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			this.connection = DriverManager
+					.getConnection("jdbc:mysql://172.16.19.136/firstRest?user=firstRest&password=lukibals");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
